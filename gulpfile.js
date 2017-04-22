a4fe3b19d4be2f8c82f3facd78194901006c7361
manpreet singh <mpsingh0001@gmail.com>
2017-04-22 17:47:50 +1200

    about me section is done. layout is responsive full.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require('gulp');
+var cleanCss = require('gulp-clean-css');
+var htmlMin = require('gulp-htmlmin');
 
 gulp.task('default', function() {
     console.log('done');
+});
+
+gulp.task('minify-css', function() {
+    gulp.src('./sass/*.css')
+        .pipe(cleanCss({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('minify-html', function() {
+    gulp.src('./index.html')
+        .pipe(htmlMin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist/html'));
 });
