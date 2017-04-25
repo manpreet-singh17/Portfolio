6f2a8d91e160905e6d4a279c48470525e487728d
manpreet singh <mpsingh0001@gmail.com>
2017-04-25 18:24:53 +1200

    all layout is done. just styling left.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var cleanCss = require('gulp-clean-css');
 var htmlMin = require('gulp-htmlmin');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
+/*======= task WATCHER=========*/
 gulp.task('default', function() {
-    console.log('done');
+    //console.log('done');
+    gulp.watch('stylesheets/**/*.scss', ['sassToCss']);
 });
 
 gulp.task('minify-css', function() {
@@ -16,4 +20,14 @@ gulp.task('minify-html', function() {
     gulp.src('./index.html')
         .pipe(htmlMin({ collapseWhitespace: true }))
         .pipe(gulp.dest('./dist/html'));
+});
+
+/*================SASS to CSS task=============== */
+gulp.task('sassToCss', function() {
+    gulp.src('stylesheets/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 4 versions']
+        }))
+        .pipe(gulp.dest('./css'));
 });
