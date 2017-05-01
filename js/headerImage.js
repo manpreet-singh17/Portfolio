7eed45dc7b01691a4e5dfd2c9f0fa17caa25edf4
manpreet singh <mpsingh0001@gmail.com>
2017-05-01 13:51:54 +1200

    header part is done.

--- js/headerImage.js
+++ js/headerImage.js
@@ -1,15 +1,1 @@
-$(function() {
-    var idsArr = ["#first", "#second"];
-
-    setInterval(function() {
-        if ($("#first").attr("class") === "show") {
-
-            $("#first").removeClass("show");
-            $("#first").removeClass("hide");
-
-        } else {
-            $("#first").removeClass("hide");
-            $("#first").addClass("show");
-        }
-    }, 1000);
-});
+$(function() {});
